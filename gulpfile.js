c9721092df450dacad7787af8802e93a54d84259
M J Chowdhury <bdjunayed@gmail.com>
2017-02-13 00:29:23 +0600

    ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,22 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync').create();
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
 //var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
 
-//var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
-    jekyllBuildMsg: '<span style="color: grey">Running:</span> $ jekyll build'
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
 /**
  * Build the Jekyll Site
  */
 gulp.task('jekyll-build', function (done) {
-    console.log('Entering jekyll-build task')
-    
-    return cp.spawn( 'jekyll' , ['build'], {stdio: 'inherit'})
+    console.log('Entering jekyll-build task');
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
         .on('close', done);
-        //exec('jekyll build', function(err, stdout, stderr) {
-        //browserSync.notify(messages.jekyllBuildMsg);
-        //browserSync.notify('jekyll build');
-        //browserSync.reload();
-    //});
 });
 
 /**
@@ -41,11 +33,10 @@ gulp.task('browser-sync', ['sass', 'scripts', 'jekyll-build'], function() {
     console.log('Entering browser-sync task')
     browserSync.init({
         server: {
-            baseDir: '_site'            
+            baseDir: './_site'            
         }
     });
 });
-
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
@@ -56,7 +47,7 @@ gulp.task('sass', function () {
             // console.error('Error!', err.message);
             // })
         //Initializes sourcemaps
-        .pipe(sourcemaps.init())
+        //.pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: ['_sass'],
             onError: browserSync.notify()}).on('error', sass.logError))
@@ -68,25 +59,17 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('assets/css'));
 });
 
+
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_sass/*.scss', ['sass']);
+    gulp.watch('./_sass/*.scss', ['sass']);
     gulp.watch('./_js/social-buttons.js', ['scripts']);
     gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_posts/*.html'], ['jekyll-rebuild']);
 });
 
-
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync & watch files.
- */
-gulp.task('default', ['browser-sync', 'watch']);
-
-
-///////////////////////// 
 
 // not enabled in task section
 gulp.task('scripts', function() {
@@ -98,3 +81,10 @@ gulp.task('scripts', function() {
   .pipe(gulp.dest('./_site/assets/js/'));
   console.log("I am here...");
 });
+
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
