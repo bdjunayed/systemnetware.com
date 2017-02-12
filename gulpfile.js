ec99c359e522eb25c866aa4acde4e13979fe2edd
M J Chowdhury <bdjunayed@gmail.com>
2017-02-13 02:46:08 +0600

    hi5 (#4)
    ok go on

--- gulpfile.js
+++ gulpfile.js
@@ -54,9 +54,8 @@ gulp.task('sass', function () {
         //.on('error', sass.logError))
         // .pipe(sass())
         //.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}));
 });
 
 
