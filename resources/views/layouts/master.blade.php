4b93f05c9fc8b1f72dfa79ab61be19d4a2ea5312
Junayed M Ch <bdjunayed@gmail.com>
2019-06-11 20:48:31 +0600

    page Title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {{-- @include('includes.meta')
+     @include('layouts.meta')
 
-    @yield('page_meta')
+    {{--@yield('page_meta')
     @yield('title') --}}
 
     <!-- Styles -->
@@ -33,9 +33,6 @@
 
 
     </div>{{-- body_background --}}
-
-{{--  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>  --}}
-{{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> --}}
 
 
 <script src="{{mix('js/manifest.js')}}"></script>
---
+++ resources/views/layouts/meta.blade.php
@@ -0,0 +1,8 @@
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#85b5bc">
+<meta name="msapplication-TileColor" content="#afd5db">
+<meta name="theme-color" content="#b2d6dc">
+<title>System Netware, Bangladesh</title>
